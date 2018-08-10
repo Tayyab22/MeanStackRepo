086b4523efc5537ff14d3ab92f021a6bffe344ff
Tom Chaudhry <tayyabji@comcast.net>
2018-08-10 10:33:21 -0400

    Home Component update

--- MeanStackRepo/client/src/app/app.module.ts
+++ MeanStackRepo/client/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app.routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MeanStackRepo/client/src/app/app.routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//import components
+import {HomeComponent} from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+
+    { path: '',   component: HomeComponent },
+    { path: 'dashboard', component: DashboardComponent},
+    { path: '**', component: HomeComponent} //should be last route.
+  ]
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+      )
+  ],
+    providers: [],
+    bootstrap: [],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+  
+
